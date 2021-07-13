71c40ee7e9fe7e7a97782e98a378720587dfbe52
hasan m.shehata <hasan.mh.sh@gmail.com>
2021-07-13 18:01:03 +0200

    Exercise
    Extend the Python Flask application with /status and /metrics endpoints, considering the following requirements:
    
        Both endpoints should return an HTTP 200 status code
        Both endpoints should return a JSON response e.g. {"user": "admin"}. (Note: the JSON response can be hardcoded at this stage)
        The /status endpoint should return a response similar to this example: result: OK - healthy
        The /metrics endpoint should return a response similar to this example: data: {UserCount: 140, UserCountActive: 23}

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+@app.route('/status')
+def get_status():
+    status = "OK -healthy"
+    return jsonify({
+        'result': status
+    })
+
+
+@app.route('/metrics')
+def get_metrics():
+    all_users = 250
+    active_users = 32
+    return jsonify({
+        'data': {
+            'UserCount': all_users, 
+            'UserCountActive': active_users
+        }
+    })
+
+    
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
+
+
